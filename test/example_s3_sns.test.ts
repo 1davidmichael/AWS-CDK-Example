d04c5272d040fac108f5c90ee1148f2fada70be2
David Michael <david.a.michael@emcins.com>
2020-06-26 13:59:18 -0500

    Add snapshot and specific resource tests

--- test/example_s3_sns.test.ts
+++ test/example_s3_sns.test.ts
@@ -1,13 +1,18 @@
-import { expect as expectCDK, MatchStyle, matchTemplate } from '@aws-cdk/assert';
+import { expect as expectCDK, MatchStyle, matchTemplate, SynthUtils } from '@aws-cdk/assert';
+import '@aws-cdk/assert/jest';
 import * as cdk from '@aws-cdk/core';
 import * as ExampleLambda from '../lib/example_s3_sns-stack';
 
-test('Empty Stack', () => {
+test('Resources', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new ExampleLambda.ExampleLambdaStack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
-    }, MatchStyle.SUPERSET))
+    }, MatchStyle.SUPERSET));
+
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
+
+    expect(stack).toHaveResource('AWS::S3::Bucket')
 });
