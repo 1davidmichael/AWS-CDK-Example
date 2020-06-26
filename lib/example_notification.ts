9be715194cbe545d2acaeec7a5170229dec1db1a
David Michael <1.david.michael@gmail.com>
2020-06-25 22:15:46 -0500

    Add migration IAM user and grant consume message permissions on queue

--- lib/example_notification.ts
+++ lib/example_notification.ts
@@ -1,6 +1,7 @@
 import * as core from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as sqs from "@aws-cdk/aws-sqs";
+import * as iam from "@aws-cdk/aws-iam";
 import * as s3n from "@aws-cdk/aws-s3-notifications";
 
 export class WidgetService extends core.Construct {
@@ -11,8 +12,14 @@ export class WidgetService extends core.Construct {
 
     const queue = new sqs.Queue(this, "Queue");
 
+    const migrationUser = new iam.User(this, "User", {
+        userName: "migrationUser"
+    });
+
     bucket.addEventNotification(s3.EventType.OBJECT_CREATED,
         new s3n.SqsDestination(queue)
     );
+
+    queue.grantConsumeMessages(migrationUser);
   }
 }
